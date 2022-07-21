e80e9ebaf2a4702dc00184a7735f6faf8c0e5680
Norge Antoni <bonitillo.noni@gmail.com>
2022-07-21 16:20:01 -0500

    CRUD productos, categorias y busqueda

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -53,14 +53,16 @@ const buscarProductos = async( termino = '', res = response) =>{
     
     const esMongoID = ObjectId.isValid(termino); //TRUE
     if (esMongoID) {
-        const producto = await Producto.findById(termino);
+        const producto = await Producto.findById(termino)
+                            .populate( 'categoria', 'nombre');
         return res.json({
             results: (producto)? [producto] : []
         });
     }
 
     const regex = new RegExp( termino, 'i');
-    const productos = await Producto.find( { nombre: regex , estado: true } );
+    const productos = await Producto.find( { nombre: regex , estado: true } )
+                            .populate( 'categoria', 'nombre');
 
     res.json({
         results: productos
